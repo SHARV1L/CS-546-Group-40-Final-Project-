ba66c6931bd064b0e71d63d422539485dbc08462
SHARV1L <livrahs@hotmail.com>
2023-03-31 10:38:12 -0400

    added CRUD function in bookings.js

--- data/bookings.js
+++ data/bookings.js
@@ -1,5 +1,32 @@
 const exportedFunctions = () => {
 
+const create = async (
+    firstName,
+    lastName,
+    email,
+    password,
+    phoneNumber,
+    profilePicture,
+    accountType,
+) => {}
+
+const getAll = async () => {}
+
+const get = async () => {}
+
+const  remove = async () => {}
+
+const update = async (
+    id,
+    firstName,
+    lastName,
+    email,
+    password,
+    phoneNumber,
+    profilePicture,
+    accountType,
+) => {}
+
 }
 
 export default exportedFunctions;
