82cd76934ce01f49c9450b0667cc860909ddd760
SHARV1L <livrahs@hotmail.com>
2023-04-10 22:03:22 -0400

    modified handlebars and routes

--- routes/listings.js
+++ routes/listings.js
@@ -2,4 +2,23 @@ import {Router} from 'express';
 
 const router = Router();
 
+// http://localhost:3000/
+router.route('/').get(async (req, res) => {
+  //code here for GET
+  try {
+    res.render('components/homepage', {title: 'Rental Finder Homepage'});
+  } catch (error) {
+    res.status(400).json({error: e});
+  }
+});
+
+// http://localhost:3000/search_rentals
+router.route('/search_rentals').get(async (req, res) => {
+    try {
+      res.render('components/listing', {title: 'Property Listing'}, );
+    } catch (error) {
+      res.status(400).json({error: e});
+    }
+  });
+
 export default router;
