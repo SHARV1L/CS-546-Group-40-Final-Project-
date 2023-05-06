6e0dfcb3946601c141ddcbf79c2e6c91c4c72dc8
Mohan Srinivas <mohansrinivas2000@gmail.com>
2023-05-06 10:33:16 -0400

    Routes error

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import bookingRoutes from './bookings.js';
 import propertyRoutes from './properties.js';
 import reviewRoutes from './reviews.js';
 import listingRoutes from './listings.js';
-import loginRoutes from './loginUser.js';
+//import loginRoutes from './loginUser.js';
 import userRoutes from './loginUser.js';
 
 const constructorMethod = (app) => {
@@ -14,12 +14,11 @@ const constructorMethod = (app) => {
   app.use('/booking', bookingRoutes);
   app.use('/property', propertyRoutes);
   app.use('/review', reviewRoutes);
-  app.user('/sign-up', loginRoutes);
   app.use('/search', listingRoutes);  // Property Listing Page route
   
 
   app.use('*', (req, res) => {
-    res.status(404).json({ error: 'Not Found' }); //render('error',  {class: 'Not Found', message: "No matching values"});
+    res.status(404).json({error : 'Not Found'}); //render('error',  {class: 'Not Found', message: "No matching values"});
   });
 };
 
